5f921111d0279e56a8ba6afe9843cfff29db3788
Margotokop67 <Margotokop67@nycmail.com>
2025-05-09 15:52:34 +0600

    Add files via upload

---
+++ .github/workflows/show.bat
@@ -0,0 +1,13 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+
+echo Facebook: https://www.facebook.com/iamjoarder
+echo Created by a Bangladeshi
+echo By Mamun Joarder
+
+echo User name : runneradmin
+echo User Pass : TheDisa1a
